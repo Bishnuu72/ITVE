c4722f66b61d9fb2d447e5b5dc52e1ded7615eb3
Bishnu Kumar Yadav <bishnuyadav857@gmail.com>
2025-11-21 02:30:47 +0545

    fixed frontend path

--- server/src/server.js
+++ server/src/server.js
@@ -49,7 +49,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: `https://itveindia.vercel.app/`, // frontend URL
+  origin: `https://itveindia.vercel.app`, // frontend URL
   credentials: true,
 }));
 
