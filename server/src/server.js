250cea5664b516baf04b23d69edebc5c020b989c
Bishnu Kumar Yadav <bishnuyadav857@gmail.com>
2025-11-21 02:52:39 +0545

    fixed app cors

--- server/src/server.js
+++ server/src/server.js
@@ -44,7 +44,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
+const app = express();
 
 // Middleware
 // app.use(express.json());
@@ -60,7 +60,6 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-const app = express();
 
 
 // ✅ NEW: Create uploads directory if it doesn't exist (fixes Multer 500 errors)
