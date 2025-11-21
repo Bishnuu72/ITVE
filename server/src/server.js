35da611cf2bc2358efc0c4334e1b6b12d0df863f
Bishnu Kumar Yadav <bishnuyadav857@gmail.com>
2025-11-21 12:02:04 +0545

    fixed express app

--- server/src/server.js
+++ server/src/server.js
@@ -45,6 +45,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(express.json());
 
 // Middleware
 // app.use(express.json());
